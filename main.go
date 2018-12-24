f47632979107449a1f2f084fcc6a4514850a92b5
dmitry kuzmin <dmitry.kuzmin@exness.com>
2018-12-24 20:25:42 +0200

    simple solution

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"./config"
-	"./rabbit"
 	"flag"
 	"github.com/juju/fslock"
 	"io/ioutil"
@@ -11,6 +9,8 @@ import (
 	"os/signal"
 	"path/filepath"
 	"time"
+	"tmp/DataDumper/config"
+	"tmp/DataDumper/rabbit"
 )
 
 type Job struct {
@@ -42,31 +42,11 @@ func main() {
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt)
 
-	s1 := make(chan *Job, 10)
-	s2 := make(chan *Job)
-	sDone := make(chan *Job)
-	finish := make(chan bool)
-
-	go stage1(s1, conf, exit, sDone)
-	go stage2(s2, s1, sDone)
-
-	go func() {
-		stage3(sDone, s2)
-		finish <- true
-	}()
-
-	log.Println("Service started.")
-
-	<-finish
-}
-
-func stage1(s1 chan<- *Job, conf *config.Config, exit chan os.Signal, sDone <-chan *Job) {
 	ticker := time.Tick(time.Second)
 	for {
 		select {
 		case <-exit:
-			close(s1)
-			return
+			log.Println("Buy-buy!")
 		case <-ticker:
 			for _, t := range conf.Tasks {
 				task := t
@@ -79,72 +59,50 @@ func stage1(s1 chan<- *Job, conf *config.Config, exit chan os.Signal, sDone <-ch
 					if f.IsDir() {
 						continue
 					}
+
 					path := t.InDir + string(os.PathSeparator) + f.Name()
-					s1 <- &Job{
+					j := &Job{
 						path,
 						fslock.New(path),
 						&task,
 					}
-					<-sDone
+
+					j.process()
 				}
 			}
 		}
 	}
 }
 
-// sometimes we have problems with locking certain files.
-// So lets lock them not blocking concurrently and push further successful
-func stage2(s2 chan<- *Job, s1 <-chan *Job, sDone chan<- *Job) {
-	s2Done := make(chan bool)
-	for i := 10; i > 0; i-- {
-		go func() {
-			for j := range s1 {
-				er := j.L.LockWithTimeout(5 * time.Second)
-				if er != nil {
-					log.Println("Error with locking file: " + j.Path)
-					sDone <- j
-					continue
-				}
-				s2 <- j
-			}
-			s2Done <- true
-		}()
+func (j *Job) process() {
+	log.Println("Sending file: " + j.Path)
+
+	er := j.L.TryLock()
+	if er != nil {
+		log.Println("Error with locking file: " + j.Path)
+		return
 	}
-	go func() {
-		for i := 10; i > 0; i-- {
-			<-s2Done
-		}
-		close(s2)
-		close(s2Done)
-	}()
-}
 
-func stage3(sDone chan<- *Job, s2 <-chan *Job) {
-	for j := range s2 {
-		log.Println("Sending file: " + j.Path)
-		if err := j.L.Unlock(); err != nil { // looks like windows is not able to read from locked file :(
-			log.Println("File unlock failed.", err.Error())
-			sDone <- j
-			continue
-		}
+	if err := j.L.Unlock(); err != nil { // looks like windows is not able to read from locked file :(
+		log.Println("File unlock failed.", err.Error())
+		return
+	}
 
-		b, err := ioutil.ReadFile(j.Path)
-		if err != nil {
-			log.Println("File read failed.", err.Error())
-			moveFailed(j)
-			sDone <- j
-			continue
-		}
+	b, err := ioutil.ReadFile(j.Path)
+	if err != nil {
+		log.Println("File read failed.", err.Error())
+		moveFailed(j)
+		return
+	}
 
-		err = rabbitConn.Publish(j.T.Queue, b)
-		if err == nil {
-			log.Println("File processed. " + j.Path)
-			moveSuccess(j)
-		} else {
-			log.Println("Send to rabbit failed. ", err.Error())
-			moveFailed(j)
-		}
-		sDone <- j
+	err = rabbitConn.Publish(j.T.Queue, b)
+
+	if err == nil {
+		log.Println("File processed. " + j.Path)
+		moveSuccess(j)
+	} else {
+		log.Println("Send to rabbit failed. ", err.Error())
+		moveFailed(j)
 	}
 }
 
